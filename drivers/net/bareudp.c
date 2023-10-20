ba1453e107a56a4cdf9a2ec6fb153402e0695797
Beniamino Galvani <b.galvani@gmail.com>
2023-10-20 13:55:26 +0200

    ipv6: remove "proto" argument from udp_tunnel6_dst_lookup()
    [ Upstream commit 7e937dcf96d0489b3cdd1cff9dfd049617d28492 ]
    
    The function is now UDP-specific, the protocol is always IPPROTO_UDP.
    
    This is similar to what already done for IPv4 in commit 78f3655adcb5
    ("ipv4: remove "proto" argument from udp_tunnel_dst_lookup()").
    
    Suggested-by: Guillaume Nault <gnault@redhat.com>
    Signed-off-by: Beniamino Galvani <b.galvani@gmail.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: be83178bfc44 ("vxlan: initialize _md in vxlan_xmit_one()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/bareudp.c
+++ drivers/net/bareudp.c
@@ -394,7 +394,7 @@ static int bareudp6_xmit_skb(struct sk_buff *skb, struct net_device *dev,
 		return -ESHUTDOWN;
 
 	dst = udp_tunnel6_dst_lookup(skb, dev, bareudp->net, sock, &saddr, info,
-				     IPPROTO_UDP, use_cache);
+				     use_cache);
 	if (IS_ERR(dst))
 		return PTR_ERR(dst);
 
@@ -524,8 +524,7 @@ static int bareudp_fill_metadata_dst(struct net_device *dev,
 			return -ESHUTDOWN;
 
 		dst = udp_tunnel6_dst_lookup(skb, dev, bareudp->net, sock,
-					     &saddr, info, IPPROTO_UDP,
-					     use_cache);
+					     &saddr, info, use_cache);
 		if (IS_ERR(dst))
 			return PTR_ERR(dst);
 
