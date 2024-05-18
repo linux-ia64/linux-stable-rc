083612461ea460809353755e1aabba75ae1672cc
Johnny Mnemonic <jm@machine-hall.org>
2024-05-18 19:38:46 +0200

    Also enable build (incl. HP Sim) and test (incl. Ski) for linux-6.9.y
    Plus:
    * Update make-kernel.bash
    * Use new kernel configuration for normal builds but keep the old one

--- tools/make-kernel.bash
+++ tools/make-kernel.bash
@@ -2,6 +2,21 @@
 
 # Frank Scheiner <frank.scheiner@web.de>
 
+# 2024-05-15 - v0.7.0
+# Add env var EMPTY_LOCALVERSION to compile with empty localversion
+
+# 2024-05-08 - v0.6.0
+# if a commit hash can't be found, ignore that part of LOCALVERSION
+
+# 2024-04-08 - v0.5.0
+# create logfile of build
+
+# 2024-03-25 - v0.4.0
+# Add System.map to the build artifacts
+
+# 2024-01-28 - v0.3.0
+# Also support other arches and do not cross compile if target arch is host arch
+
 # 2023-10-23 - v0.2.0
 # Use shortened SHA1 hashes in kernel release to allow for more space for `flavor`
 
@@ -12,7 +27,7 @@ _debug=1
 # Config
 _ramdisk="/usr/src/ramdisk"
 
-_supportedArches=( "alpha" "ia64" )
+_supportedArches=( "alpha" "ia64" "x86_64" )
 
 _threads=$( nproc )
 
@@ -34,6 +49,15 @@ debug()
 	else
 		:
 	fi
+
+	return
+}
+
+compressLogFile()
+{
+	local _logFile="$1"
+
+	zstdmt "${_logFile}" &>/dev/null &
 
 	return
 }
@@ -99,6 +123,30 @@ if [[ $_archIsSupported -eq 0 ]]; then
 	exit 1
 fi
 
+_hostArch=$( uname -m )
+
+if [[ ${_hostArch} == ${_arch} ]]; then
+
+	_crossCompileOption=""
+else
+	# determine cross compiler tuples
+	if hash ${_arch}-unknown-linux-gnu-gcc &>/dev/null; then
+
+		_crossCompileOption="CROSS_COMPILE=${_arch}-unknown-linux-gnu-"
+
+	elif hash ${_arch}-linux-gnu-gcc &>/dev/null; then
+
+		_crossCompileOption="CROSS_COMPILE=${_arch}-linux-gnu-"
+
+	elif hash ${_arch}-linux-gcc &>/dev/null; then
+
+		_crossCompileOption="CROSS_COMPILE=${_arch}-linux-"
+	else
+		echo "$0: Can't find cross compiler. Cannot continue."
+		exit 1
+	fi
+fi
+
 if [[ -d ${_kernelTree} ]]; then
 
 	#echo "Copying kernel tree to RAMdisk..."
@@ -120,6 +168,11 @@ fi
 
 ####
 
+# start of logged operations
+_tempLogFile=$( mktemp )
+echo "START: $( date )" | tee -a "$_tempLogFile"
+
+(
 echo "Copying kernel config..."
 debug "cp ${_kernelConfig} ${_kernelTree}/.config"
 cp ${_kernelConfig} ${_kernelTree}/.config
@@ -131,15 +184,33 @@ else
 	echo "failed"
 	exit 1
 fi
+) | tee -a "$_tempLogFile"
 
 _oldPWD="$PWD"
 cd ${_kernelTree} || ( echo "$0: cannot cd to \"${_kernelTree}\"."; exit 1 )
 
-# olddefconfig
+# define LOCALVERSION
+_currentCommitHashShort=$( git rev-parse --short HEAD )
 
+if [[ "$_currentCommitHashShort" == "" ]]; then
+
+	_localversionCurrentCommit=""
+else
+	_localversionCurrentCommit="-${_currentCommitHashShort}"
+fi
+
+_localversion="${_localversionCurrentCommit}-${_arch}${_flavor}"
+
+if [[ $EMPTY_LOCALVERSION -eq 1 ]]; then
+
+	_localversion=""
+fi
+
+# make olddefconfig ############################################################
+(
 echo "Configuring kernel..."
-debug "make LOCALVERSION=\"-$( git rev-parse --short HEAD )-${_arch}${_flavor}\" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- olddefconfig"
-make LOCALVERSION="-$( git rev-parse --short HEAD )-${_arch}${_flavor}" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- olddefconfig
+debug "make LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} olddefconfig"
+make LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} olddefconfig
 
 if [[ $? -eq 0 ]]; then
 
@@ -148,13 +219,15 @@ else
 	echo "failed"
 	exit 1
 fi
+) 2>&1 | tee -a "$_tempLogFile"
 
-# all
 
+# make all #####################################################################
+(
 echo "Making kernel..."
-debug "time make -j${_threads} LOCALVERSION=\"-$( git rev-parse --short HEAD )-${_arch}${_flavor}\" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- all"
+debug "time make -j${_threads} LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} all"
 date
-time make -j${_threads} LOCALVERSION="-$( git rev-parse --short HEAD )-${_arch}${_flavor}" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- all
+time make -j${_threads} LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} all
 _makeReturned=$?
 echo ${_makeReturned}
 date
@@ -166,18 +239,17 @@ else
 	echo "failed"
 	exit 1
 fi
+) 2>&1 | tee -a "$_tempLogFile"
 
-cp vmlinux "../vmlinux${_flavor}"
-cp vmlinux.gz "../vmlinux.gz${_flavor}"
-
+# exit early for CI run
 exit
 
-# modules_install
-
+# make modules_install #########################################################
+(
 echo "Installing modules..."
-debug "time make LOCALVERSION=\"-$( git rev-parse --short HEAD )-${_arch}${_flavor}\" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- modules_install"
+debug "time make LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} modules_install"
 date
-time make LOCALVERSION="-$( git rev-parse --short HEAD )-${_arch}${_flavor}" ARCH=${_arch} CROSS_COMPILE=${_arch}-linux- modules_install
+time make LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} modules_install
 _makeReturned=$?
 echo ${_makeReturned}
 date
@@ -189,10 +261,11 @@ else
 	echo "failed"
 	exit 1
 fi
+) 2>&1 | tee -a "$_tempLogFile"
 
-_kernelRelease=$( make -s LOCALVERSION="-$( git rev-parse --short HEAD )-${_arch}${_flavor}" kernelrelease )
 
-# save artifacts
+# save artifacts ###############################################################
+_kernelRelease=$( make -s LOCALVERSION="${_localversion}" kernelrelease )
 cp vmlinux /boot/vmlinux-${_kernelRelease}
 
 _uncompressedKernel="/boot/vmlinux-${_kernelRelease}"
@@ -204,14 +277,21 @@ if [[ "${_arch}" == "alpha" ]]; then
 	gzip -c -9 vmlinux > vmlinuz
 	_compressedKernel="/boot/vmlinuz-${_kernelRelease}-stripped"
 	cp vmlinuz "${_compressedKernel}"
+
+elif [[ "${_arch}" == "x86_64" ]]; then
+
+	_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
+	cp arch/x86/boot/bzImage "${_compressedKernel}"
 else
 	_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
 	cp vmlinux.gz "${_compressedKernel}"
 fi
 
-cp .config /boot/config-${_kernelRelease}
-
 _configuration="/boot/config-${_kernelRelease}"
+cp .config ${_configuration}
+
+_systemMap="/boot/System.map-${_kernelRelease}"
+cp System.map ${_systemMap}
 
 cd /lib/modules
 
@@ -231,15 +311,25 @@ else
 	_kernelModules=( "/lib/modules/${_kernelRelease}.tar" )
 fi
 
-cd ${_oldPWD}
+_finalLogFile="/boot/build.log-${_kernelRelease}"
 
+cd ${_oldPWD}
+(
 echo "Build artifacts:"
+echo "${_finalLogFile}.zst"
 echo "${_configuration}"
+echo "${_systemMap}"
 echo "${_uncompressedKernel}"
 echo "${_compressedKernel}"
 for _tarball in "${_kernelModules[@]}"; do
 
 	echo "${_tarball}"
 done
+echo "END: $( date )"
+) | tee -a "$_tempLogFile"
+
+mv "$_tempLogFile" "$_finalLogFile"
+
+compressLogFile "$_finalLogFile" &
 
 exit
