de9b00005c97a54af9e0e15ec159b8b1f377e265
Johnny Mnemonic <jm@machine-hall.org>
2024-08-02 22:40:45 +0200

    Make regular kernel build artifacts available for download
    * artifact zip file contains:
      * zstd compressed build log
      * zstd compressed tarball of kernel images and modules
      * `SHA256` with hash values of the files above
    * retention time is 2 days
    
    > **NOTICE:** Unfortunately only registered users can download artifacts
    > (GH limitation).

--- tools/make-kernel.bash
+++ tools/make-kernel.bash
@@ -2,6 +2,9 @@
 
 # Frank Scheiner <frank.scheiner@web.de>
 
+# 2024-08-03 - v0.9.0
+# make kernel tarball if needed
+
 # 2024-05-15 - v0.7.0
 # Add env var EMPTY_LOCALVERSION to compile with empty localversion
 
@@ -36,7 +39,7 @@ _flavor=""
 
 usageMsg()
 {
-	echo "Usage: $0 <KERNEL_CONFIG> <ARCH> <KERNEL_TREE> [<FLAVOR>]"
+	echo "Usage: $0 <KERNEL_CONFIG> <ARCH> <KERNEL_TREE> [<FLAVOR> [tar-pkg]]"
 
 	return
 }
@@ -95,6 +98,12 @@ if [[ "$1" != "" ]]; then
 
 	_flavor="-${1}"
 	shift
+
+	if [[ "$1" != "" ]]; then
+
+		_extraMakeOption="$1"
+		shift
+	fi
 fi
 
 ####
@@ -186,6 +195,11 @@ else
 fi
 ) | tee -a "$_tempLogFile"
 
+if [[ ! $? -eq 0 ]]; then
+
+        exit 1
+fi
+
 _oldPWD="$PWD"
 cd ${_kernelTree} || ( echo "$0: cannot cd to \"${_kernelTree}\"."; exit 1 )
 
@@ -221,119 +235,204 @@ else
 fi
 ) 2>&1 | tee -a "$_tempLogFile"
 
+if [[ ! $? -eq 0 ]]; then
 
-# make all #####################################################################
-(
-echo "Making kernel..."
-debug "time make -j${_threads} LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} all"
-date
-time make -j${_threads} LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} all
-_makeReturned=$?
-echo ${_makeReturned}
-date
-
-if [[ ${_makeReturned} -eq 0 ]]; then
-
-	echo "done"
-else
-	echo "failed"
 	exit 1
 fi
-) 2>&1 | tee -a "$_tempLogFile"
 
-# copy kernels to the expected place
-cp vmlinux "../vmlinux${_flavor}"
-cp vmlinux.gz "../vmlinux.gz${_flavor}"
-
-# exit early for CI run
-exit
-
-# make modules_install #########################################################
-(
-echo "Installing modules..."
-debug "time make LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} modules_install"
-date
-time make LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} modules_install
-_makeReturned=$?
-echo ${_makeReturned}
-date
-
-if [[ ${_makeReturned} -eq 0 ]]; then
-
-	echo "done"
-else
-	echo "failed"
-	exit 1
-fi
-) 2>&1 | tee -a "$_tempLogFile"
-
-
-# save artifacts ###############################################################
 _kernelRelease=$( make -s LOCALVERSION="${_localversion}" kernelrelease )
-cp vmlinux /boot/vmlinux-${_kernelRelease}
 
-_uncompressedKernel="/boot/vmlinux-${_kernelRelease}"
+_tarball="linux-${_kernelRelease}.tar"
+_finalLogFileName="build.log-${_kernelRelease}"
 
-if [[ "${_arch}" == "alpha" ]]; then
+if [[ "$_extraMakeOption" != "" ]]; then
 
-	# Due to aboot being unable to handle arbitrary large kernel and initrd images (total max size roughly 13 MiB or so!)
-	alpha-linux-strip vmlinux
-	gzip -c -9 vmlinux > vmlinuz
-	_compressedKernel="/boot/vmlinuz-${_kernelRelease}-stripped"
-	cp vmlinuz "${_compressedKernel}"
+	# make tarball #####################################################################
+	(
+	echo "Making kernel tarball..."
+	debug "time make -j${_threads} LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} ${_extraMakeOption}"
+	date
+	time make -j${_threads} LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} ${_extraMakeOption}
+	_makeReturned=$?
+	echo ${_makeReturned}
+	date
 
-elif [[ "${_arch}" == "x86_64" ]]; then
+	if [[ ${_makeReturned} -eq 0 ]]; then
 
-	_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
-	cp arch/x86/boot/bzImage "${_compressedKernel}"
+		echo "done"
+	else
+		echo "failed"
+		exit 1
+	fi
+	) 2>&1 | tee -a "$_tempLogFile"
+
+	if [[ ! $? -eq 0 ]]; then
+
+	        exit 1
+	fi
+
+	# replace vmlinuz (which is just a copy of the unstripped and uncompressed vmlinux) with vmlinux.gz
+	_vmlinuz="tar-install/boot/vmlinuz-${_kernelRelease}"
+	echo "cp vmlinux.gz \"$_vmlinuz\"" | tee -a "$_tempLogFile"
+	cp vmlinux.gz "$_vmlinuz"
+
+	rm -f tar-install/boot/*kbuild*
+
+	if [[ -e ${_tarball} ]]; then
+
+		rm -f ${_tarball}
+	fi
+
+	# make actual tarball
+	pushd tar-install &>/dev/null
+	(
+	echo "time tar -cf ../${_tarball} boot lib"
+	date
+	time tar -cf ../${_tarball} boot lib
+	date
+	) 2>&1 | tee -a "$_tempLogFile"
+	popd &>/dev/null
+
+	# compress tarball
+	(
+	echo "zstd -T0 -9 ${_tarball}"
+	date
+	time zstd -T0 -9 ${_tarball} &>/dev/null
+	date
+	) 2>&1 | tee -a "$_tempLogFile"
+
+	(
+	echo "Build artifacts:"
+	echo "$PWD/${_finalLogFileName}.zst"
+	echo "$PWD/${_tarball}.zst"
+	echo "END: $( date )"
+	) | tee -a "$_tempLogFile"
+
+	zstd -T0 -9 "$_tempLogFile" -o "${_finalLogFileName}.zst" &>/dev/null && rm "$_tempLogFile"
+
+	echo "$PWD/${_finalLogFileName}.zst" > LINUX_BUILD_LOG
+	echo "$PWD/${_tarball}.zst" > LINUX_TARBALL
 else
-	_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
-	cp vmlinux.gz "${_compressedKernel}"
+	# make all #####################################################################
+	(
+	echo "Making kernel..."
+	debug "time make -j${_threads} LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} all"
+	date
+	time make -j${_threads} LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} all
+	_makeReturned=$?
+	echo ${_makeReturned}
+	date
+
+	if [[ ${_makeReturned} -eq 0 ]]; then
+
+		echo "done"
+	else
+		echo "failed"
+		exit 1
+	fi
+	) 2>&1 | tee -a "$_tempLogFile"
+
+	if [[ ! $? -eq 0 ]]; then
+
+		exit 1
+	fi
+
+	if [[ "$_flavor" == "ski" ]]; then
+
+		# If building a kernel to run in ski exit here already
+		exit
+	fi
+
+	# make modules_install #########################################################
+	(
+	echo "Installing modules..."
+	debug "time make LOCALVERSION=\"${_localversion}\" ARCH=${_arch} ${_crossCompileOption} modules_install"
+	date
+	time make LOCALVERSION="${_localversion}" ARCH=${_arch} ${_crossCompileOption} modules_install
+	_makeReturned=$?
+	echo ${_makeReturned}
+	date
+
+	if [[ ${_makeReturned} -eq 0 ]]; then
+
+		echo "done"
+	else
+		echo "failed"
+		exit 1
+	fi
+	) 2>&1 | tee -a "$_tempLogFile"
+
+	if [[ ! $? -eq 0 ]]; then
+
+		exit 1
+	fi
+	# save artifacts ###############################################################
+	cp vmlinux /boot/vmlinux-${_kernelRelease}
+
+	_uncompressedKernel="/boot/vmlinux-${_kernelRelease}"
+
+	if [[ "${_arch}" == "alpha" ]]; then
+
+		# Due to aboot being unable to handle arbitrary large kernel and initrd images (total max size roughly 13 MiB or so!)
+		alpha-linux-strip vmlinux
+		gzip -c -9 vmlinux > vmlinuz
+		_compressedKernel="/boot/vmlinuz-${_kernelRelease}-stripped"
+		cp vmlinuz "${_compressedKernel}"
+
+	elif [[ "${_arch}" == "x86_64" ]]; then
+
+		_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
+		cp arch/x86/boot/bzImage "${_compressedKernel}"
+	else
+		_compressedKernel="/boot/vmlinuz-${_kernelRelease}"
+		cp vmlinux.gz "${_compressedKernel}"
+	fi
+
+	_configuration="/boot/config-${_kernelRelease}"
+	cp .config ${_configuration}
+
+	_systemMap="/boot/System.map-${_kernelRelease}"
+	cp System.map ${_systemMap}
+
+	cd /lib/modules
+
+	if [[ "${_arch}" == "alpha" ]]; then
+
+		# same here (see above)
+		tar -cf ${_kernelRelease}-non-stripped.tar ${_kernelRelease}
+		cd ${_kernelRelease}
+		find . -name "*.ko" -exec alpha-linux-strip --strip-debug '{}' \;
+		cd ..
+		tar -cf ${_kernelRelease}-stripped.tar ${_kernelRelease}
+
+		_kernelModules=( "/lib/modules/${_kernelRelease}-non-stripped.tar" "/lib/modules/${_kernelRelease}-stripped.tar" )
+	else
+		tar -cf ${_kernelRelease}.tar ${_kernelRelease}
+
+		_kernelModules=( "/lib/modules/${_kernelRelease}.tar" )
+	fi
+
+	_finalLogFile="/boot/build.log-${_kernelRelease}"
+
+	cd ${_oldPWD}
+	(
+	echo "Build artifacts:"
+	echo "${_finalLogFile}.zst"
+	echo "${_configuration}"
+	echo "${_systemMap}"
+	echo "${_uncompressedKernel}"
+	echo "${_compressedKernel}"
+	for _tarball in "${_kernelModules[@]}"; do
+
+		echo "${_tarball}"
+	done
+	echo "END: $( date )"
+	) | tee -a "$_tempLogFile"
+
+	mv "$_tempLogFile" "$_finalLogFile"
+
+	compressLogFile "$_finalLogFile"
+
 fi
-
-_configuration="/boot/config-${_kernelRelease}"
-cp .config ${_configuration}
-
-_systemMap="/boot/System.map-${_kernelRelease}"
-cp System.map ${_systemMap}
-
-cd /lib/modules
-
-if [[ "${_arch}" == "alpha" ]]; then
-
-	# same here (see above)
-	tar -cf ${_kernelRelease}-non-stripped.tar ${_kernelRelease}
-	cd ${_kernelRelease}
-	find . -name "*.ko" -exec alpha-linux-strip --strip-debug '{}' \;
-	cd ..
-	tar -cf ${_kernelRelease}-stripped.tar ${_kernelRelease}
-
-	_kernelModules=( "/lib/modules/${_kernelRelease}-non-stripped.tar" "/lib/modules/${_kernelRelease}-stripped.tar" )
-else
-	tar -cf ${_kernelRelease}.tar ${_kernelRelease}
-
-	_kernelModules=( "/lib/modules/${_kernelRelease}.tar" )
-fi
-
-_finalLogFile="/boot/build.log-${_kernelRelease}"
-
-cd ${_oldPWD}
-(
-echo "Build artifacts:"
-echo "${_finalLogFile}.zst"
-echo "${_configuration}"
-echo "${_systemMap}"
-echo "${_uncompressedKernel}"
-echo "${_compressedKernel}"
-for _tarball in "${_kernelModules[@]}"; do
-
-	echo "${_tarball}"
-done
-echo "END: $( date )"
-) | tee -a "$_tempLogFile"
-
-mv "$_tempLogFile" "$_finalLogFile"
-
-compressLogFile "$_finalLogFile" &
 
 exit
