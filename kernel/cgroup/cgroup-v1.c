6bd7dd47dd2b6ab320e473d394a55a2d9af04829
Michal Koutný <mkoutny@suse.com>
2024-09-09 18:32:22 +0200

    cgroup: Disallow mounting v1 hierarchies without controller implementation
    [ Upstream commit 3c41382e920f1dd5c9f432948fe799c07af1cced ]
    
    The configs that disable some v1 controllers would still allow mounting
    them but with no controller-specific files. (Making such hierarchies
    equivalent to named v1 hierarchies.) To achieve behavior consistent with
    actual out-compilation of a whole controller, the mounts should treat
    respective controllers as non-existent.
    
    Wrap implementation into a helper function, leverage legacy_files to
    detect compiled out controllers. The effect is that mounts on v1 would
    fail and produce a message like:
      [ 1543.999081] cgroup: Unknown subsys name 'memory'
    
    Signed-off-by: Michal Koutný <mkoutny@suse.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -52,6 +52,12 @@ bool cgroup1_ssid_disabled(int ssid)
 	return cgroup_no_v1_mask & (1 << ssid);
 }
 
+static bool cgroup1_subsys_absent(struct cgroup_subsys *ss)
+{
+	/* Check also dfl_cftypes for file-less controllers, i.e. perf_event */
+	return ss->legacy_cftypes == NULL && ss->dfl_cftypes;
+}
+
 /**
  * cgroup_attach_task_all - attach task 'tsk' to all cgroups of task 'from'
  * @from: attach to all cgroups of a given task
@@ -941,7 +947,8 @@ int cgroup1_parse_param(struct fs_context *fc, struct fs_parameter *param)
 			return 0;
 		}
 		for_each_subsys(ss, i) {
-			if (strcmp(param->key, ss->legacy_name))
+			if (strcmp(param->key, ss->legacy_name) ||
+			    cgroup1_subsys_absent(ss))
 				continue;
 			if (!cgroup_ssid_enabled(i) || cgroup1_ssid_disabled(i))
 				return invalf(fc, "Disabled controller '%s'",
@@ -1027,7 +1034,8 @@ static int check_cgroupfs_options(struct fs_context *fc)
 	mask = ~((u16)1 << cpuset_cgrp_id);
 #endif
 	for_each_subsys(ss, i)
-		if (cgroup_ssid_enabled(i) && !cgroup1_ssid_disabled(i))
+		if (cgroup_ssid_enabled(i) && !cgroup1_ssid_disabled(i) &&
+		    !cgroup1_subsys_absent(ss))
 			enabled |= 1 << i;
 
 	ctx->subsys_mask &= enabled;
