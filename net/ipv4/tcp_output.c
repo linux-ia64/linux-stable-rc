665046512d891cdca513db675e8ce24ad0df73c6
Jason Xing <kerneljasonxing@gmail.com>
2025-02-20 15:29:31 +0800

    bpf: Prevent unsafe access to the sock fields in the BPF timestamping callback
    [ Upstream commit fd93eaffb3f977b23bc0a48d4c8616e654fcf133 ]
    
    The subsequent patch will implement BPF TX timestamping. It will
    call the sockops BPF program without holding the sock lock.
    
    This breaks the current assumption that all sock ops programs will
    hold the sock lock. The sock's fields of the uapi's bpf_sock_ops
    requires this assumption.
    
    To address this, a new "u8 is_locked_tcp_sock;" field is added. This
    patch sets it in the current sock_ops callbacks. The "is_fullsock"
    test is then replaced by the "is_locked_tcp_sock" test during
    sock_ops_convert_ctx_access().
    
    The new TX timestamping callbacks added in the subsequent patch will
    not have this set. This will prevent unsafe access from the new
    timestamping callbacks.
    
    Potentially, we could allow read-only access. However, this would
    require identifying which callback is read-safe-only and also requires
    additional BPF instruction rewrites in the covert_ctx. Since the BPF
    program can always read everything from a socket (e.g., by using
    bpf_core_cast), this patch keeps it simple and disables all read
    and write access to any socket fields through the bpf_sock_ops
    UAPI from the new TX timestamping callback.
    
    Moreover, note that some of the fields in bpf_sock_ops are specific
    to tcp_sock, and sock_ops currently only supports tcp_sock. In
    the future, UDP timestamping will be added, which will also break
    this assumption. The same idea used in this patch will be reused.
    Considering that the current sock_ops only supports tcp_sock, the
    variable is named is_locked_"tcp"_sock.
    
    Signed-off-by: Jason Xing <kerneljasonxing@gmail.com>
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://patch.msgid.link/20250220072940.99994-4-kerneljasonxing@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -522,6 +522,7 @@ static void bpf_skops_hdr_opt_len(struct sock *sk, struct sk_buff *skb,
 		sock_owned_by_me(sk);
 
 		sock_ops.is_fullsock = 1;
+		sock_ops.is_locked_tcp_sock = 1;
 		sock_ops.sk = sk;
 	}
 
@@ -567,6 +568,7 @@ static void bpf_skops_write_hdr_opt(struct sock *sk, struct sk_buff *skb,
 		sock_owned_by_me(sk);
 
 		sock_ops.is_fullsock = 1;
+		sock_ops.is_locked_tcp_sock = 1;
 		sock_ops.sk = sk;
 	}
 
