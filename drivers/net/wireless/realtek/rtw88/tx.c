39ba544af83fc5442ef4bebe0c203b5303785113
Bitterblue Smith <rtl8821cerfe2@gmail.com>
2026-03-18 19:45:13 +0200

    wifi: rtw88: TX QOS Null data the same way as Null data
    [ Upstream commit 737e980e12983bb7420a2c00b981a1e607079a84 ]
    
    When filling out the TX descriptor, Null data frames are treated like
    management frames, but QOS Null data frames are treated like normal
    data frames. Somehow this causes a problem for the firmware.
    
    When connected to a network in the 2.4 GHz band, wpa_supplicant (or
    NetworkManager?) triggers a scan every five minutes. During these scans
    mac80211 transmits many QOS Null frames in quick succession. Because
    these frames are marked with IEEE80211_TX_CTL_REQ_TX_STATUS, rtw88
    asks the firmware to report the TX ACK status for each of these frames.
    Sometimes the firmware can't process the TX status requests quickly
    enough, they add up, it only processes some of them, and then marks
    every subsequent TX status report with the wrong number.
    
    The symptom is that after a while the warning "failed to get tx report
    from firmware" appears every five minutes.
    
    This problem apparently happens only with the older RTL8723D, RTL8821A,
    RTL8812A, and probably RTL8703B chips.
    
    Treat QOS Null data frames the same way as Null data frames. This seems
    to avoid the problem.
    
    Tested with RTL8821AU, RTL8723DU, RTL8811CU, and RTL8812BU.
    
    Signed-off-by: Bitterblue Smith <rtl8821cerfe2@gmail.com>
    Acked-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Link: https://patch.msgid.link/2b53fb0d-b1ed-47b6-8caa-2bb9ae2acb80@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/realtek/rtw88/tx.c
+++ drivers/net/wireless/realtek/rtw88/tx.c
@@ -416,7 +416,7 @@ void rtw_tx_pkt_info_update(struct rtw_dev *rtwdev,
 		vif = si->vif;
 	}
 
-	if (ieee80211_is_mgmt(fc) || ieee80211_is_nullfunc(fc))
+	if (ieee80211_is_mgmt(fc) || ieee80211_is_any_nullfunc(fc))
 		rtw_tx_mgmt_pkt_info_update(rtwdev, pkt_info, sta, skb);
 	else if (ieee80211_is_data(fc))
 		rtw_tx_data_pkt_info_update(rtwdev, pkt_info, sta, skb);
