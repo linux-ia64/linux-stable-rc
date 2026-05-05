582352f5d4019f706b2820724cec93b4e56de484
Viacheslav Dubeyko <slava@dubeyko.com>
2026-05-05 15:00:52 -0700

    hfsplus: rework hfsplus_readdir() logic
    [ Upstream commit 4b0496432844628ad05a5b1efce329a3340174d2 ]
    
    The xfstests' test-case generic/637 fails with error:
    
    FSTYP -- hfsplus
    PLATFORM -- Linux/x86_64 hfsplus-testing-0001 6.15.0-rc4+ #8 SMP PREEMPT_DYNAMIC Thu May 1 16:43:22 PDT 2025
    MKFS_OPTIONS -- /dev/loop51
    MOUNT_OPTIONS -- /dev/loop51 /mnt/scratch
    
    QA output created by 637
    entries 7 and 8 have duplicate d_off 8
    Found unlinked files in open dir (see xfstests-dev/results//generic/637.full for details)
    
    Debugging of the hfsplus_readdir() logic showed this:
    
    hfsplus: hfsplus_readdir(): 163 ctx->pos 0
    hfsplus: hfsplus_readdir(): 189 ctx->pos 1
    hfsplus: hfsplus_readdir(): 264 ctx->pos 2, ino 18
    hfsplus: hfsplus_readdir(): 264 ctx->pos 3, ino 19
    hfsplus: hfsplus_readdir(): 264 ctx->pos 4, ino 28
    hfsplus: hfsplus_readdir(): 264 ctx->pos 5, ino 118
    hfsplus: hfsplus_readdir(): 264 ctx->pos 6, ino 29
    hfsplus: hfsplus_readdir(): 264 ctx->pos 7, ino 30
    hfsplus: hfsplus_readdir(): 264 ctx->pos 8, ino 31
    hfsplus: hfsplus_readdir(): 304 ctx->pos 8
    hfsplus: hfsplus_unlink():420 dir->i_ino 17, inode->i_ino 28
    hfsplus: hfsplus_readdir(): 141 ctx->pos 7
    hfsplus: hfsplus_readdir(): 264 ctx->pos 7, ino 31
    hfsplus: hfsplus_readdir(): 264 ctx->pos 8, ino 32
    hfsplus: hfsplus_readdir(): 264 ctx->pos 9, ino 33
    
    It means that hfsplus_readdir() stopped the processing of
    folder's items on ctx->pos 8, then, item with ino 28 has
    been deleted and hfsplus_readdir() re-started the logic
    from ctx->pos 7. As a result, previous and new sets of
    folder's items have overlapping values for the case of
    d_off 8.
    
    Currently, HFS+ has very complicated and fragile logic
    of rd->file->f_pos correction in hfsplus_delete_cat().
    This patch removes this logic and it stores the current
    pos into hfsplus_readdir_data. Finally, if rd->pos == ctx->pos
    then hfsplus_readdir() tries to find the position in
    b-tree's node by means of hfsplus_cat_key. This position is
    used to re-start the folder's content traversal.
    
    sudo ./check generic/637
    FSTYP         -- hfsplus
    PLATFORM      -- Linux/x86_64 hfsplus-testing-0001 7.1.0-rc1+ #44 SMP PREEMPT_DYNAMIC Mon May  4 15:58:45 PDT 2026
    MKFS_OPTIONS  -- /dev/loop51
    MOUNT_OPTIONS -- /dev/loop51 /mnt/scratch
    
    generic/637  22s ...  22s
    Ran: generic/637
    Passed all 1 tests
    
    Closes: https://github.com/hfs-linux-kernel/hfs-linux-kernel/issues/198
    cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    cc: Yangtao Li <frank.li@vivo.com>
    cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Viacheslav Dubeyko <slava@dubeyko.com>
    Link: https://lore.kernel.org/r/20260505220051.2854696-2-slava@dubeyko.com
    Signed-off-by: Viacheslav Dubeyko <slava@dubeyko.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/hfsplus/inode.c
+++ fs/hfsplus/inode.c
@@ -435,8 +435,6 @@ struct inode *hfsplus_new_inode(struct super_block *sb, struct inode *dir,
 	inode->i_mtime = inode->i_atime = inode->i_ctime = current_time(inode);
 
 	hip = HFSPLUS_I(inode);
-	INIT_LIST_HEAD(&hip->open_dir_list);
-	spin_lock_init(&hip->open_dir_lock);
 	mutex_init(&hip->extents_lock);
 	atomic_set(&hip->opencnt, 0);
 	hip->extent_state = 0;
