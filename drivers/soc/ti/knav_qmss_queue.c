8a3860e41db446b4eb245409d993c4a71fcdc3b4
Md Shofiqul Islam <shofiqtest@gmail.com>
2026-05-06 18:41:14 +0300

    soc: ti: knav_qmss_queue: Implement resource cleanup in remove()
    [ Upstream commit 10a1969353b20caa50c320717e054601631c0d3e ]
    
    Implement the TODO in knav_queue_remove() by stopping PDSPs and
    freeing queue regions and queue ranges before disabling runtime PM,
    mirroring the cleanup performed in the probe error path.
    
    Set device_ready to false before cleanup to prevent any further
    use of the device during teardown.
    
    This ensures resources are released on driver unbind and avoids
    leaking queue/region state.
    
    Suggested-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Md Shofiqul Islam <shofiqtest@gmail.com>
    Link: https://lore.kernel.org/linux-arm-kernel/20260506154114.2288-1-shofiqtest@gmail.com/
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Stable-dep-of: 3c8178627599 ("soc: ti: knav_qmss: Remove debugfs file on teardown")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/soc/ti/knav_qmss_queue.c
+++ drivers/soc/ti/knav_qmss_queue.c
@@ -1886,7 +1886,12 @@ err:
 
 static int knav_queue_remove(struct platform_device *pdev)
 {
-	/* TODO: Free resources */
+	struct knav_device *kdev = platform_get_drvdata(pdev);
+
+	device_ready = false;
+	knav_queue_stop_pdsps(kdev);
+	knav_queue_free_regions(kdev);
+	knav_free_queue_ranges(kdev);
 	pm_runtime_put_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 	return 0;
