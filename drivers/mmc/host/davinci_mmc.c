d9a4d7b61e2cc516a737ec45a92e3f0d7693257b
Osama Abdelkader <osama.abdelkader@gmail.com>
2026-05-10 18:29:39 +0200

    mmc: davinci: fix mmc_add_host order in probe
    [ Upstream commit d04e0151d316edbdb4f0397a9b92a1936e4a1421 ]
    
    mmc_add_host() makes the host visible to the MMC core. Register the
    interrupt handlers and advertise MMC_CAP_SDIO_IRQ before that, so the
    core cannot start using the host before IRQ handling is set up.
    
    Signed-off-by: Osama Abdelkader <osama.abdelkader@gmail.com>
    Signed-off-by: Ulf Hansson <ulfh@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mmc/host/davinci_mmc.c
+++ drivers/mmc/host/davinci_mmc.c
@@ -1303,14 +1303,10 @@ static int davinci_mmcsd_probe(struct platform_device *pdev)
 		goto cpu_freq_fail;
 	}
 
-	ret = mmc_add_host(mmc);
-	if (ret < 0)
-		goto mmc_add_host_fail;
-
 	ret = devm_request_irq(&pdev->dev, irq, mmc_davinci_irq, 0,
 			       mmc_hostname(mmc), host);
 	if (ret)
-		goto request_irq_fail;
+		goto mmc_add_host_fail;
 
 	if (host->sdio_irq >= 0) {
 		ret = devm_request_irq(&pdev->dev, host->sdio_irq,
@@ -1320,6 +1316,10 @@ static int davinci_mmcsd_probe(struct platform_device *pdev)
 			mmc->caps |= MMC_CAP_SDIO_IRQ;
 	}
 
+	ret = mmc_add_host(mmc);
+	if (ret < 0)
+		goto mmc_add_host_fail;
+
 	rename_region(mem, mmc_hostname(mmc));
 
 	dev_info(mmc_dev(host->mmc), "Using %s, %d-bit mode\n",
@@ -1328,8 +1328,6 @@ static int davinci_mmcsd_probe(struct platform_device *pdev)
 
 	return 0;
 
-request_irq_fail:
-	mmc_remove_host(mmc);
 mmc_add_host_fail:
 	mmc_davinci_cpufreq_deregister(host);
 cpu_freq_fail:
