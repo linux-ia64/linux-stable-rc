8124a430dc06fe30ada4b7a3b547605fb63c9838
Maurizio Lombardi <mlombard@redhat.com>
2026-05-14 10:32:53 +0200

    nvme-core: align fabrics_q teardown with admin_q in nvme_free_ctrl
    [ Upstream commit f702badaf7d31dc3dea6c66da92b5f35fadd89dc ]
    
    Currently, the final reference for the fabrics admin queue (fabrics_q)
    is dropped inside nvme_remove_admin_tag_set(). However, the primary admin
    queue (admin_q) defers dropping its final reference until
    nvme_free_ctrl().
    
    Move the blk_put_queue() call for fabrics_q from
    nvme_remove_admin_tag_set() to nvme_free_ctrl(). This aligns the
    lifecycle management of both admin queues, ensuring they are freed
    symmetrically when the controller is finally torn down.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Hannes Reinecke <hare@kernel.org>
    Reviewed-by: Sagi Grimberg <sagi@grimberg.me>
    Reviewed-by: Daniel Wagner <dwagner@suse.de>
    Signed-off-by: Maurizio Lombardi <mlombard@redhat.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -4325,10 +4325,8 @@ EXPORT_SYMBOL_GPL(nvme_alloc_admin_tag_set);
 void nvme_remove_admin_tag_set(struct nvme_ctrl *ctrl)
 {
 	blk_mq_destroy_queue(ctrl->admin_q);
-	if (ctrl->ops->flags & NVME_F_FABRICS) {
+	if (ctrl->fabrics_q)
 		blk_mq_destroy_queue(ctrl->fabrics_q);
-		blk_put_queue(ctrl->fabrics_q);
-	}
 	blk_mq_free_tag_set(ctrl->admin_tagset);
 }
 EXPORT_SYMBOL_GPL(nvme_remove_admin_tag_set);
@@ -4471,6 +4469,8 @@ static void nvme_free_ctrl(struct device *dev)
 
 	if (ctrl->admin_q)
 		blk_put_queue(ctrl->admin_q);
+	if (ctrl->fabrics_q)
+		blk_put_queue(ctrl->fabrics_q);
 	if (!subsys || ctrl->instance != subsys->instance)
 		ida_free(&nvme_instance_ida, ctrl->instance);
 
