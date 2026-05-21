cb7c70b8017d52852ee29cabc6bce97d3f0ec664
Hans Zhang <18255117159@163.com>
2026-05-22 00:18:18 +0800

    PCI: iproc: Protect root bus removal with rescan lock
    [ Upstream commit a6a64e150f12ad5391e0a0d60f6a3d119b06ce50 ]
    
    Hold the pci_rescan_remove_lock lock while stopping and removing a root bus
    to avoid racing with concurrent rescan or hotplug operations triggered via
    sysfs.  Such races may lead to use-after-free issues or system crashes.
    
    Signed-off-by: Hans Zhang <18255117159@163.com>
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>
    [bhelgaas: commit log]
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Link: https://patch.msgid.link/20260521161822.132996-6-18255117159@163.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/controller/pcie-iproc.c
+++ drivers/pci/controller/pcie-iproc.c
@@ -1556,8 +1556,10 @@ int iproc_pcie_remove(struct iproc_pcie *pcie)
 {
 	struct pci_host_bridge *host = pci_host_bridge_from_priv(pcie);
 
+	pci_lock_rescan_remove();
 	pci_stop_root_bus(host->bus);
 	pci_remove_root_bus(host->bus);
+	pci_unlock_rescan_remove();
 
 	iproc_pcie_msi_disable(pcie);
 
