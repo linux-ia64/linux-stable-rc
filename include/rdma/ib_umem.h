24fa69d1a893af88c50a0330ab6f82eddea6ede8
Jason Gunthorpe <jgg@nvidia.com>
2026-06-01 13:52:32 -0300

    RDMA/umem: Be careful about boundary conditions in ib_umem_find_best_pgsz()
    [ Upstream commit 09ea6837a0434fb4db99528a5055b6d822135dcf ]
    
    Several corner cases, especially important on 32 bits:
    
    - umem->iova is u64, the function argument should pass in u64 or
      iova will be truncated
    - Check that the length is not too large for the iova
    - Check that lengths > 4G don't overflow the GENMASK
    
    Link: https://patch.msgid.link/r/2-v1-88303e9e509f+f7-ib_umem_types_jgg@nvidia.com
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/rdma/ib_umem.h
+++ include/rdma/ib_umem.h
@@ -79,7 +79,7 @@ int ib_umem_copy_from(void *dst, struct ib_umem *umem, size_t offset,
 		      size_t length);
 unsigned long ib_umem_find_best_pgsz(struct ib_umem *umem,
 				     unsigned long pgsz_bitmap,
-				     unsigned long virt);
+				     u64 virt);
 
 /**
  * ib_umem_find_best_pgoff - Find best HW page size
@@ -139,7 +139,7 @@ static inline int ib_umem_copy_from(void *dst, struct ib_umem *umem, size_t offs
 }
 static inline unsigned long ib_umem_find_best_pgsz(struct ib_umem *umem,
 						   unsigned long pgsz_bitmap,
-						   unsigned long virt)
+						   u64 virt)
 {
 	return 0;
 }
