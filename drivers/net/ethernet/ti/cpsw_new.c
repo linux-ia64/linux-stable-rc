e2980c88829da959212263c4d05ae44943ef137e
Guangshuo Li <lgs201920130244@gmail.com>
2026-06-04 12:31:15 +0800

    net: cpsw_new: unregister devlink on port registration failure
    [ Upstream commit b64f763b607426ac97e44b114f0b8844ac3b86dd ]
    
    cpsw_probe() registers devlink before registering the CPSW ports.
    
    If cpsw_register_ports() fails, the error path only unregisters the
    notifiers and then releases the lower level resources. It does not undo
    the successful cpsw_register_devlink() call, leaving the devlink instance
    and its parameters registered after probe has failed.
    
    Add a devlink cleanup label for the path where devlink registration has
    already succeeded, and use it when port registration fails.
    
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Alexander Sverdlin <alexander.sverdlin@siemens.com>
    Signed-off-by: Guangshuo Li <lgs201920130244@gmail.com>
    Link: https://patch.msgid.link/20260604043115.1409134-1-lgs201920130244@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/ti/cpsw_new.c
+++ drivers/net/ethernet/ti/cpsw_new.c
@@ -2029,7 +2029,7 @@ skip_cpts:
 
 	ret = cpsw_register_ports(cpsw);
 	if (ret)
-		goto clean_unregister_notifiers;
+		goto clean_unregister_devlink;
 
 	dev_notice(dev, "initialized (regs %pa, pool size %d) hw_ver:%08X %d.%d (%d)\n",
 		   &ss_res->start, descs_pool_size,
@@ -2041,6 +2041,8 @@ skip_cpts:
 
 	return 0;
 
+clean_unregister_devlink:
+	cpsw_unregister_devlink(cpsw);
 clean_unregister_notifiers:
 	cpsw_unregister_notifiers(cpsw);
 clean_cpts:
