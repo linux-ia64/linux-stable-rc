afb2395cfe2fad4a3a30a5fbb805adad7dd010d1
Vadim Fedorenko <vadim.fedorenko@linux.dev>
2026-06-11 19:03:33 +0000

    ptp: ocp: add shutdown callback
    [ Upstream commit f6f955cbf9d4e02deebe54ca91c118b53be9ffe6 ]
    
    The shutdown callback was never implemented for this driver, but it's
    needed because .remove() callback is never called during kexec/reboot
    process. That leaves HW with some interrupts enabled and may cause
    spurious interrupt while booting into a new kernel during with kexec.
    If it happens that I2C interrupt fires during kexec, the whole I2C bus
    is disabled leaving TimeCard with no devlink communication. The same
    happens if timestampers were enabled, leaving the card without
    timestamper interrupts until full reboot cycle.
    
    Implement .shutdown() callback with the same function as remove
    callback.
    
    Signed-off-by: Vadim Fedorenko <vadim.fedorenko@linux.dev>
    Link: https://patch.msgid.link/20260611190333.787132-1-vadim.fedorenko@linux.dev
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/ptp/ptp_ocp.c
+++ drivers/ptp/ptp_ocp.c
@@ -1506,6 +1506,7 @@ static struct pci_driver ptp_ocp_driver = {
 	.id_table	= ptp_ocp_pcidev_id,
 	.probe		= ptp_ocp_probe,
 	.remove		= ptp_ocp_remove,
+	.shutdown	= ptp_ocp_remove,
 };
 
 static int
