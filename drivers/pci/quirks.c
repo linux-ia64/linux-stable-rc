9d78f9469b424823aec2ad743ae3012dec1c52dd
Max Lee <max.lee@canonical.com>
2026-07-07 10:15:27 +0800

    PCI/ASPM: Avoid L0s for Realtek RTS525A
    commit ec3d987fcaf92516d13ee18c305c82281557046d upstream.
    
    The Realtek RTS525A PCIe card reader reports an AER Correctable Replay
    Timer Timeout storm when ASPM L0s is enabled on its link.  On an affected
    HP ZBook Power 16 inch G11, the Root Port received tens of millions of AER
    interrupts from the RTS525A even when the rtsx_pci driver was blacklisted
    and the endpoint was not enabled by a driver.
    
    For example:
    
      pcieport 0000:00:1c.6: AER: Multiple Correctable error message received from 0000:58:00.0
      rtsx_pci 0000:58:00.0: PCIe Bus Error: severity=Correctable, type=Data Link Layer, (Transmitter ID)
      rtsx_pci 0000:58:00.0:   device [10ec:525a] error status/mask=00001000/00006000
      rtsx_pci 0000:58:00.0:    [12] Timeout
      pcieport 0000:00:1c.6: AER: Correctable error message received from 0000:58:00.0
    
    Testing with OS-native AER control showed that disabling only L0s on the
    RTS525A link stops new AER interrupt and counter growth while leaving L1
    enabled.  Disabling L1, L1 substates, or Clock PM alone did not stop the
    storm.
    
    Prevent the broken L0s configuration by removing L0s from the RTS525A
    advertised ASPM capability.  This avoids enabling the non-working ASPM
    state instead of masking the resulting AER Replay Timer Timeout reports.
    
    Signed-off-by: Max Lee <max.lee@canonical.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Reviewed-by: Lukas Wunner <lukas@wunner.de>
    Reviewed-by: Manivannan Sadhasivam <mani@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20260707021527.639611-1-max.lee@canonical.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -2350,6 +2350,9 @@ DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_INTEL, 0x10f1, quirk_disable_aspm_l0s);
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_INTEL, 0x10f4, quirk_disable_aspm_l0s);
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_INTEL, 0x1508, quirk_disable_aspm_l0s);
 
+/* Realtek RTS525A generates a Replay Timer Timeout storm when L0s is enabled. */
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_REALTEK, 0x525a, quirk_disable_aspm_l0s);
+
 static void quirk_disable_aspm_l0s_l1(struct pci_dev *dev)
 {
 	pci_info(dev, "Disabling ASPM L0s/L1\n");
