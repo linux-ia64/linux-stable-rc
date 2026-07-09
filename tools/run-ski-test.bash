5393bc3365feae276bf7dfa81e3480f056b3d98d
Johnny Mnemonic <jm@machine-hall.org>
2026-07-09 14:12:44 +0200

    Improve run-ski-test.bash to avoid no output for Ski running 7.x kernels
    It's still unclear, why this only happens for 7.x kernel builds but it
    happened there consistently since starting to also build 7.x kernels.
    
    Tested to work fine locally, hopefully this will work similarly on GHA.

--- tools/run-ski-test.bash
+++ tools/run-ski-test.bash
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-export SKI_USE_FAKE_XTERM=1
+set -x
 
 _skiBootLoader="$1"
 _hpSimLinuxKernel="$2"
+
 _skiPathVar="/bin:/sbin:/usr/bin:/usr/sbin"
+_skiLog="ski-$$.log"
+_skiInit="/root/bin/ski_test.bash"
+
 
 bski \
+	-conslog "${_skiLog}" \
 	${_skiBootLoader} \
 	${_hpSimLinuxKernel} \
 	root=/dev/sda \
 	simscsi=./sd \
 	simeth=tap0 \
-	init=/root/bin/ski_test.bash \
+	init=${_skiInit} \
 	PATH=${_skiPathVar} \
 	rw \
 	memblock=debug &
@@ -21,16 +26,18 @@ _skiPID=$!
 
 sleep 0.25
 
-_skiLog=ski.*
-
 tail -f -n +1 ${_skiLog} &
 
 _tailPID=$!
+
+set +x
 
 while ! grep 'INFO: Ski execution finished.' ${_skiLog} &>/dev/null; do
 
 	sleep 10
 done
+
+set -x
 
 sleep 5
 
@@ -39,9 +46,7 @@ kill ${_skiPID}
 
 if grep 'INFO: Ski test succeeded.' ${_skiLog} &>/dev/null; then
 
-	rm ${_skiLog}
 	exit 0
 else
-	rm ${_skiLog}
 	exit 1
 fi
