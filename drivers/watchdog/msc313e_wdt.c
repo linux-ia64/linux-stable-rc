162859e41714c1d8c5f702adba3b55d3f8b839ff
Tzung-Bi Shih <tzungbi@kernel.org>
2026-08-27 04:46:59 +0000

    watchdog: msc313e: Fix NULL pointer dereference in PM callbacks
    [ Upstream commit e3eceb76515910746e6268c4e4ac1c07516ebd7b ]
    
    msc313e_wdt_probe() doesn't set the driver data for the platform device.
    As a result, dev_get_drvdata() in msc313e_wdt_suspend() and
    msc313e_wdt_resume() will return NULL, leading to a NULL pointer
    dereference afterward.
    
    Set the platform device driver data in msc313e_wdt_probe().
    
    Fixes: e9800b799464 ("watchdog: Add Mstar MSC313e WDT driver")
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Link: https://patch.msgid.link/20260827044700.554333-2-tzungbi@kernel.org
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/watchdog/msc313e_wdt.c
+++ drivers/watchdog/msc313e_wdt.c
@@ -121,6 +121,7 @@ static int msc313e_wdt_probe(struct platform_device *pdev)
 	priv->wdev.timeout = MSC313E_WDT_DEFAULT_TIMEOUT;
 
 	watchdog_set_drvdata(&priv->wdev, priv);
+	platform_set_drvdata(pdev, priv);
 
 	watchdog_init_timeout(&priv->wdev, timeout, dev);
 	watchdog_stop_on_reboot(&priv->wdev);
