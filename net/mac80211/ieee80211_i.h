a5932f6432c9df9fcdb569e57024ad38563ae56f
Johannes Berg <johannes.berg@intel.com>
2026-09-04 16:57:10 +0200

    wifi: mac80211: suppress chanctx warning for debugfs reset
    [ Upstream commit bf29d085e0eba92388518719d044f4702a8c6644 ]
    
    Before suspend all the channel contexts should removed, so the
    warning makes sense and should be there, but during reset the
    same code is called without first removing. Limit the check to
    the real suspend case.
    
    Assisted-by: LLM
    Fixes: 12e7f517029d ("mac80211: cleanup generic suspend/resume procedures")
    Reported-by: syzbot+56a1a45a9a2c04d425ff@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=56a1a45a9a2c04d425ff
    Link: https://patch.msgid.link/20260904165722.fe46395e310b.Ic4aaa95bd9d0ceb6a3cd7d84c425afee7d7d3dd7@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/mac80211/ieee80211_i.h
+++ net/mac80211/ieee80211_i.h
@@ -2161,7 +2161,7 @@ int ieee80211_reconfig(struct ieee80211_local *local);
 void ieee80211_stop_device(struct ieee80211_local *local);
 
 int __ieee80211_suspend(struct ieee80211_hw *hw,
-			struct cfg80211_wowlan *wowlan);
+			struct cfg80211_wowlan *wowlan, bool reset);
 
 static inline int __ieee80211_resume(struct ieee80211_hw *hw)
 {
