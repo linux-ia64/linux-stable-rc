da670d53493da1f4d39abdb548da600e77912201
Vincent Guittot <vincent.guittot@linaro.org>
2026-09-07 14:38:55 +0200

    sched/eevdf: Fix augmented max_slice
    commit 9a8bc9bb4c3fb3218b4f151f98a722fbeb5b5c34 upstream.
    
    Similarly to se->min_slice, init se->max_slice with se->slice before
    enqueueing the entity so the augmented callback computes it correctly
    at parent level.
    
    Fixes: 6e3c0a4e1ad1 ("sched/fair: Fix lag clamp")
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://patch.msgid.link/20260907123855.1297976-1-vincent.guittot@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -887,6 +887,8 @@ static void __enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 	avg_vruntime_add(cfs_rq, se);
 	se->min_vruntime = se->vruntime;
 	se->min_slice = se->slice;
+	se->max_slice = se->slice;
+
 	rb_add_augmented_cached(&se->run_node, &cfs_rq->tasks_timeline,
 				__entity_less, &min_vruntime_cb);
 }
