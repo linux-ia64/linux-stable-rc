6cfabb7bb7b3fdf743b10589c244a25ded3ef745
Tzung-Bi Shih <tzungbi@kernel.org>
2026-09-14 09:47:29 +0000

    Bluetooth: btmtksdio: Fix PM runtime reference leak in shutdown
    [ Upstream commit 7b60ee5f46f2ee329de661f7c68b6818d8136220 ]
    
    In btmtksdio_shutdown(), pm_runtime_get_sync() is called at the
    beginning of the function.  However, if sending the WMT function
    control command fails later, the driver returns early.
    
    It bypasses the corresponding pm_runtime_put_noidle() and
    pm_runtime_disable() calls, leaking the PM usage counter and leaving PM
    runtime enabled indefinitely.
    
    Fall through to execute the PM runtime cleanup block even if WMT errors.
    
    Fixes: 7f3c563c575e ("Bluetooth: btmtksdio: Add runtime PM support to SDIO based Bluetooth")
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/bluetooth/btmtksdio.c
+++ drivers/bluetooth/btmtksdio.c
@@ -898,10 +898,8 @@ static int btmtksdio_shutdown(struct hci_dev *hdev)
 	wmt_params.status = NULL;
 
 	err = mtk_hci_wmt_sync(hdev, &wmt_params);
-	if (err < 0) {
+	if (err < 0)
 		bt_dev_err(hdev, "Failed to send wmt func ctrl (%d)", err);
-		return err;
-	}
 
 	pm_runtime_put_noidle(bdev->dev);
 	pm_runtime_disable(bdev->dev);
