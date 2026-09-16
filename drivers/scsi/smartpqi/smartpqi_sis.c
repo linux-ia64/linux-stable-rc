31d714f4c2ff8f793cc76fc56c6e16e6a2ea54b6
Sasha Levin <sashal@kernel.org>
2026-09-15 20:08:01 -0400

    Revert "scsi: smartpqi: Fix BUILD_BUG_ON() statements"
    This reverts commit e434879e7e0880de501f745550671cb71a556bc8.
    
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/smartpqi/smartpqi_sis.c
+++ drivers/scsi/smartpqi/smartpqi_sis.c
@@ -479,7 +479,7 @@ int sis_wait_for_fw_triage_completion(struct pqi_ctrl_info *ctrl_info)
 	return rc;
 }
 
-void sis_verify_structures(void)
+static void __attribute__((unused)) verify_structures(void)
 {
 	BUILD_BUG_ON(offsetof(struct sis_base_struct,
 		revision) != 0x0);
