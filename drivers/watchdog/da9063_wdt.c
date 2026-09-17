ff13c3a6e9eced83db91548e63fab4f33b2e42a1
Li Jun <lijun01@kylinos.cn>
2026-09-17 09:37:10 +0800

    watchdog: da9063: fix suspend/resume handling of HW_RUNNING watchdog
    commit 7cb575b71ab98194d2e040bded3a7281e089c5ed upstream.
    
    da9063_wdt_suspend() and da9063_wdt_resume() only check watchdog_active(),
    when the watchdog is left running by the driver sets
    WDOG_HW_RUNNING in da9063_wdt_probe() but userspace never opens the
    device, so WDOG_ACTIVE remains cleared, the wdt_disable() will not be
    executed in da9063_wdt_suspend. In this case, the suspend callback is
    a no-op and the watchdog keeps counting during system suspend,
    leading to an unexpected system reset.
    Check WDOG_HW_RUNNING and wdd，can fix this issue.
    
    Fixes: a7ceca4398bc8 ("watchdog: da9063: optionally disable watchdog during suspend")
    Cc: stable@vger.kernel.org
    Signed-off-by: Li Jun <lijun01@kylinos.cn>
    Link: https://patch.msgid.link/20260917013710.2754679-1-lijun01@kylinos.cn
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/watchdog/da9063_wdt.c
+++ drivers/watchdog/da9063_wdt.c
@@ -262,7 +262,7 @@ static int __maybe_unused da9063_wdt_suspend(struct device *dev)
 	if (!use_sw_pm)
 		return 0;
 
-	if (watchdog_active(wdd))
+	if (watchdog_active(wdd) || watchdog_hw_running(wdd))
 		return da9063_wdt_stop(wdd);
 
 	return 0;
@@ -275,7 +275,7 @@ static int __maybe_unused da9063_wdt_resume(struct device *dev)
 	if (!use_sw_pm)
 		return 0;
 
-	if (watchdog_active(wdd))
+	if (watchdog_active(wdd) || watchdog_hw_running(wdd))
 		return da9063_wdt_start(wdd);
 
 	return 0;
