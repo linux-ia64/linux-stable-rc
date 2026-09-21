e0b242c972c9ca949b9be6325e68075ee84c7889
Chengming Zhou <zhouchengming@bytedance.com>
2026-09-21 16:07:26 -0400

    blk-mq: fix tags UAF when shrinking q->nr_hw_queues
    [ Upstream commit 6be6d112419713334ddd9c01f219ca16adaa4c76 ]
    
    When nr_hw_queues shrink, we free the excess tags before realloc'ing
    hw_ctxs for each queue. During that resize, we may need to access those
    tags, like blk_mq_tag_idle(hctx) will access queue shared tags.
    
    This can cause a slab use-after-free, as reported by KASAN. Fix it by
    moving the releasing of excess tags to the end.
    
    Fixes: e1dd7bc93029 ("blk-mq: fix tags leak when shrink nr_hw_queues")
    Reported-by: Yi Zhang <yi.zhang@redhat.com>
    Closes: https://lore.kernel.org/all/CAHj4cs_CK63uoDpGBGZ6DN4OCTpzkR3UaVgK=LX8Owr8ej2ieQ@mail.gmail.com/
    Cc: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Link: https://lore.kernel.org/r/20230908005702.2183908-1-chengming.zhou@linux.dev
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    [ Backport to 6.1.y follow-up to e1dd7bc93029: move those frees after
      hctx resizing using cur_nr_hw_queues, preventing its tag UAF. ]
    Assisted-by: LLM
    Signed-off-by: Artem Dinaburg <artem@trailofbits.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -4509,13 +4509,9 @@ static int blk_mq_realloc_tag_set_tags(struct blk_mq_tag_set *set,
 				  int cur_nr_hw_queues, int new_nr_hw_queues)
 {
 	struct blk_mq_tags **new_tags;
-	int i;
 
-	if (cur_nr_hw_queues >= new_nr_hw_queues) {
-		for (i = new_nr_hw_queues; i < cur_nr_hw_queues; i++)
-			__blk_mq_free_map_and_rqs(set, i);
+	if (cur_nr_hw_queues >= new_nr_hw_queues)
 		return 0;
-	}
 
 	new_tags = kcalloc_node(new_nr_hw_queues, sizeof(struct blk_mq_tags *),
 				GFP_KERNEL, set->numa_node);
@@ -4804,7 +4800,8 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 {
 	struct request_queue *q;
 	LIST_HEAD(head);
-	int prev_nr_hw_queues;
+	int prev_nr_hw_queues = set->nr_hw_queues;
+	int i;
 
 	lockdep_assert_held(&set->tag_list_lock);
 
@@ -4831,7 +4828,6 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 		blk_mq_sysfs_unregister_hctxs(q);
 	}
 
-	prev_nr_hw_queues = set->nr_hw_queues;
 	if (blk_mq_realloc_tag_set_tags(set, set->nr_hw_queues, nr_hw_queues) <
 	    0)
 		goto reregister;
@@ -4869,6 +4865,10 @@ switch_back:
 
 	list_for_each_entry(q, &set->tag_list, tag_set_list)
 		blk_mq_unfreeze_queue(q);
+
+	/* Free the excess tags when nr_hw_queues shrink. */
+	for (i = set->nr_hw_queues; i < prev_nr_hw_queues; i++)
+		__blk_mq_free_map_and_rqs(set, i);
 }
 
 void blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set, int nr_hw_queues)
