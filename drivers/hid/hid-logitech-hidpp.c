22f8184c218fd0913a2623cfc854fcd2af3ef9c7
Benoît Sevens <bsevens@google.com>
2026-09-22 08:16:14 +0000

    HID: logitech-hidpp: fix race condition when accessing stale stack pointer
    commit e2aaf2d3ad92ac4a8afa6b69ad4c38e7747d3d6e upstream.
    
    The driver uses hidpp->send_receive_buf to point to a stack-allocated
    buffer in the synchronous command path (__do_hidpp_send_message_sync).
    However, this pointer is not cleared when the function returns.
    
    If an event is processed (e.g. by a different thread) while the
    send_mutex is held by a new command, but before that command has
    updated send_receive_buf, the handler (hidpp_raw_hidpp_event) will
    observe that the mutex is locked and dereference the stale pointer.
    
    This results in an out-of-bounds access on a different thread's kernel
    stack (or a NULL pointer dereference on the very first command).
    
    Fix this by:
    1. Clearing hidpp->send_receive_buf to NULL before releasing the mutex
       in the synchronous command path.
    2. Moving the assignment of the local 'question' and 'answer' pointers
       inside the mutex_is_locked() block in the handler, and adding
       a NULL check before dereferencing.
    
    Fixes: 2f31c5252910 ("HID: Introduce hidpp, a module to handle Logitech hid++ devices")
    Cc: stable@vger.kernel.org
    Signed-off-by: Benoît Sevens <bsevens@google.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    [Lee: Clear hidpp->send_receive_buf at exit label in hidpp_send_message_sync()
          as __do_hidpp_send_message_sync() was split out later in 60165ab774cb]
    Signed-off-by: Lee Jones <lee@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hid/hid-logitech-hidpp.c
+++ drivers/hid/hid-logitech-hidpp.c
@@ -306,6 +306,7 @@ static int hidpp_send_message_sync(struct hidpp_device *hidpp,
 	}
 
 exit:
+	hidpp->send_receive_buf = NULL;
 	mutex_unlock(&hidpp->send_mutex);
 	return ret;
 
@@ -3308,8 +3309,7 @@ static int hidpp_input_configured(struct hid_device *hdev,
 static int hidpp_raw_hidpp_event(struct hidpp_device *hidpp, u8 *data,
 		int size)
 {
-	struct hidpp_report *question = hidpp->send_receive_buf;
-	struct hidpp_report *answer = hidpp->send_receive_buf;
+	struct hidpp_report *question, *answer;
 	struct hidpp_report *report = (struct hidpp_report *)data;
 	int ret;
 
@@ -3318,6 +3318,12 @@ static int hidpp_raw_hidpp_event(struct hidpp_device *hidpp, u8 *data,
 	 * previously sent command.
 	 */
 	if (unlikely(mutex_is_locked(&hidpp->send_mutex))) {
+		question = hidpp->send_receive_buf;
+		answer = hidpp->send_receive_buf;
+
+		if (!question)
+			return 0;
+
 		/*
 		 * Check for a correct hidpp20 answer or the corresponding
 		 * error
