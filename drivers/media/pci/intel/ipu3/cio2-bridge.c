d1ff671bd16ac3d506342f534b17004675a8d499
Hans de Goede <hdegoede@redhat.com>
2026-09-24 13:33:12 -0400

    media: ipu-bridge: Fix null pointer deref on SSDB/PLD parsing warnings
    [ Upstream commit 284be5693163343e1cf17c03917eecd1d6681bcf ]
    
    When ipu_bridge_parse_rotation() and ipu_bridge_parse_orientation() run
    sensor->adev is not set yet.
    
    So if either of the dev_warn() calls about unknown values are hit this
    will lead to a NULL pointer deref.
    
    Set sensor->adev earlier, with a borrowed ref to avoid making unrolling
    on errors harder, to fix this.
    
    Fixes: 485aa3df0dff ("media: ipu3-cio2: Parse sensor orientation and rotation")
    Cc: Fabian Wüthrich <me@fabwu.ch>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    
    [ Backport to 6.1.y: apply the sensor adev assignment in this tree's
      older ipu3/cio2-bridge.c and cio2_bridge_connect_sensor(). ]
    Assisted-by: LLM
    Signed-off-by: Artem Dinaburg <artem@trailofbits.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/media/pci/intel/ipu3/cio2-bridge.c
+++ drivers/media/pci/intel/ipu3/cio2-bridge.c
@@ -280,6 +280,11 @@ static int cio2_bridge_connect_sensor(const struct cio2_sensor_config *cfg,
 		}
 
 		sensor = &bridge->sensors[bridge->n_sensors];
+		/*
+		 * Borrow our adev ref to the sensor for now, on success
+		 * acpi_dev_get(adev) is done further below.
+		 */
+		sensor->adev = adev;
 		strscpy(sensor->name, cfg->hid, sizeof(sensor->name));
 
 		ret = cio2_bridge_read_acpi_buffer(adev, "SSDB",
