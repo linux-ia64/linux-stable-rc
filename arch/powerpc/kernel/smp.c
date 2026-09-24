bf5e207a242b716b02c109cd5543d8b65666c74e
ndesaulniers@google.com <ndesaulniers@google.com>
2026-09-24 13:30:59 -0400

    start_kernel: Add __no_stack_protector function attribute
    [ Upstream commit 514ca14ed5444b911de59ed3381dfd195d99fe4b ]
    
    Back during the discussion of
    commit a9a3ed1eff36 ("x86: Fix early boot crash on gcc-10, third try")
    we discussed the need for a function attribute to control the omission
    of stack protectors on a per-function basis; at the time Clang had
    support for no_stack_protector but GCC did not. This was fixed in
    gcc-11. Now that the function attribute is available, let's start using
    it.
    
    Callers of boot_init_stack_canary need to use this function attribute
    unless they're compiled with -fno-stack-protector, otherwise the canary
    stored in the stack slot of the caller will differ upon the call to
    boot_init_stack_canary. This will lead to a call to __stack_chk_fail()
    then panic.
    
    Link: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=94722
    Link: https://lore.kernel.org/all/20200316130414.GC12561@hirez.programming.kicks-ass.net/
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Acked-by: Michael Ellerman <mpe@ellerman.id.au> (powerpc)
    Acked-by: Miguel Ojeda <ojeda@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Link: https://lore.kernel.org/r/20230412-no_stackp-v2-1-116f9fe4bbe7@google.com
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    
    Signed-off-by: ndesaulniers@google.com <ndesaulniers@google.com>
    
    [ Backport to 6.1.y: retain this tree's start_kernel() signature, which
      predates its upstream __noreturn annotation, and add
      __no_stack_protector. ]
    Assisted-by: LLM
    Signed-off-by: Artem Dinaburg <artem@trailofbits.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -1608,6 +1608,7 @@ static void add_cpu_to_masks(int cpu)
 }
 
 /* Activate a secondary processor. */
+__no_stack_protector
 void start_secondary(void *unused)
 {
 	unsigned int cpu = raw_smp_processor_id();
--- init/main.c
+++ init/main.c
@@ -908,7 +908,8 @@ static void __init print_unknown_bootoptions(void)
 	memblock_free(unknown_options, len);
 }
 
-asmlinkage __visible void __init __no_sanitize_address start_kernel(void)
+asmlinkage __visible __init __no_sanitize_address __no_stack_protector
+void start_kernel(void)
 {
 	char *command_line;
 	char *after_dashes;
